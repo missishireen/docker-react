2a8ade865fdba292cda74e3c1e54e357b6da0c1e
ShereenMessi <shereenmessi@hotmail.com>
2020-09-19 23:10:30 +0100

    changed the code

--- src/App.js
+++ src/App.js
@@ -15,8 +15,7 @@ function App() {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Learn React
-        </a>
+                    I was changed on the feature branch        </a>
             </header>
         </div>
     );
